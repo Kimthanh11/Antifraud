b2c8fe85c4cfadc1b2aaddd49accb1678702b716
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-08-24 17:55:22 +0700

    Commit stage 1,2,3

--- Anti-Fraud System/task/src/antifraud/service/UserDetailsServiceImpl.java
+++ Anti-Fraud System/task/src/antifraud/service/UserDetailsServiceImpl.java
@@ -26,7 +26,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found "));
 
         return new UserDetailsImpl(user);
     }
